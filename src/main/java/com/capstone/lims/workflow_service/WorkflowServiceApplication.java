2b0edd8e621b8b2ef28d8592408fdf1506accb86
Hrishikesh Shelke <dev.hrishi@outlook.com>
2025-03-22 17:09:01 +0530

    established db connection, tested with eureka connection

--- src/main/java/com/capstone/lims/workflow_service/WorkflowServiceApplication.java
+++ src/main/java/com/capstone/lims/workflow_service/WorkflowServiceApplication.java
@@ -2,8 +2,10 @@ package com.capstone.lims.workflow_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class WorkflowServiceApplication {
 
 	public static void main(String[] args) {
